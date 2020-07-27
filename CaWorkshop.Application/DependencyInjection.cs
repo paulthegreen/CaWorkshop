d163099af3bbdf028e35daa297b4fd295c92d893
Paul Hellsten <hellsten.p@wehi.edu.au>
2020-07-27 15:44:10 +1000

    Beyond the Basics 01 - Architecture & Design

---
+++ CaWorkshop.Application/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CaWorkshop.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(
+            this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
--- CaWorkshop.WebUI/Controllers/TodoListsController.cs
+++ CaWorkshop.WebUI/Controllers/TodoListsController.cs
@@ -1,12 +1,10 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using CaWorkshop.WebUI.Data;
-using CaWorkshop.WebUI.Models;
+using CaWorkshop.Infrastructure.Persistence;
+using CaWorkshop.Domain.Entities;
 
 namespace CaWorkshop.WebUI.Controllers
 {
